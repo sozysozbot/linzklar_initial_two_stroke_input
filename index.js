50c905ff3bed419523fde471fc8b5b7343496bc4
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2025-03-16 05:00:55 +0900

    Add key_pmcp and key_xon to the layout

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ function restoreInitialKeys() {
         for (let y = 0; y < 4; y++) {
             const key = document.getElementById(`x${x}y${y}`);
             const icon_name = [
-                "1_処.svg", "1_下.svg", "1_六.svg", "1_人.svg", "1_ナ而.svg", "1_一ノ.svg", "1_一？.svg", "",
+                "1_処.svg", "1_下.svg", "1_六.svg", "1_人.svg", "1_ナ而.svg", "1_一ノ.svg", "1_一？.svg", "key_pmcp.png",
                 "2_上.svg", "2_二.svg", "2_右.svg", "2_言日.svg", "2_口.svg", "2_筆.svg", "2_門.svg", "2_函包箱.svg",
                 "3_ノ一.svg", "3_常.svg", "3_ノノ.svg", "3_之.svg", "3_四.svg", "3_ヒクカ丹.svg", "3_天.svg", "3_神十位.svg",
-                "", "4_火心.svg", "4_再.svg", "4_ヽヽ.svg", "5_反.svg", "5_フ.svg", "5_傾.svg", "5_針.svg"
+                "key_xon.png", "4_火心.svg", "4_再.svg", "4_ヽヽ.svg", "5_反.svg", "5_フ.svg", "5_傾.svg", "5_針.svg"
             ][y * 8 + x];
             key.innerHTML = icon_name !== "" ? `<img width="72" height="72" src="folder_icons/${icon_name}">` : "";
             key.onclick = () => firstStroke({ 'x': x, 'y': y });
@@ -42,7 +42,7 @@ function insertCharacter(characterToInsert) {
 
     // Note: we must not check whether the document.activeElement is textarea
     // because the textarea is not focused when the user clicks on the keyboard
-    
+
     const start = textarea.selectionStart;
     const end = textarea.selectionEnd;
 
