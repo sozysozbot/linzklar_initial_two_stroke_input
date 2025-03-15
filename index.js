b22548bdc61c14c61917357c51742be659c32d90
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2025-03-16 06:10:15 +0900

    Add images and implement keys

--- index.js
+++ index.js
@@ -21,6 +21,50 @@ function linmarn_afterFirstStroke(pos) {
     }
 }
 
+function pmcp_displayInitialKeys() {
+    for (let x = 0; x < 8; x++) {
+        for (let y = 0; y < 4; y++) {
+            const key = document.getElementById(`x${x}y${y}`);
+            const icon_name = [
+                "key_kin.png", "normal_keys/key_p.png", "normal_keys/key_b.png", "normal_keys/key_m.png", "normal_keys/key_c.png", "normal_keys/key_x.png", "normal_keys/key_z.png", "字.svg",
+                "", "normal_keys/key_t.png", "normal_keys/key_d.png", "normal_keys/key_n.png", "normal_keys/key_l.png", "normal_keys/key_k.png", "normal_keys/key_a.png", "special_keys/key_left.png",
+                "special_keys/key_anp.png", "normal_keys/key_e.png", "normal_keys/key_i.png", "normal_keys/key_u.png", "normal_keys/key_o.png", "normal_keys/key_j.png", "normal_keys/key_w.png", "special_keys/key_right.png",
+                "key_xon.png", "normal_keys/key_period.png", "normal_keys/key_comma.png", "normal_keys/key_question.png", "normal_keys/key_exclamation.png", "normal_keys/key_hyphen.png", "normal_keys/key_double_quote.png", "special_keys/key_laiju_e_lucuc.png",
+            ][y * 8 + x];
+            if (icon_name === "字.svg") {
+                key.innerHTML = `<img width="72" height="72" src="folder_icons/${icon_name}">`;
+                key.onclick = () => linmarn_displayInitialKeys();
+            } else if (icon_name.match(/normal_keys\/key_(.+)\.png/)) {
+                const char_ = /normal_keys\/key_(.+)\.png/.exec(icon_name)[1];
+                const char = {
+                    "period": ".",
+                    "comma": ",",
+                    "question": "?",
+                    "exclamation": "!",
+                    "hyphen": "-",
+                    "double_quote": "\"",
+                }[char_] ?? char_;
+                key.innerHTML = `<img width="72" height="72" src="folder_icons/${icon_name}">`;
+                key.onclick = () => {
+                    console.log(char);
+                    insertCharacter(char);
+                    pmcp_displayInitialKeys();
+                };
+            } else if (icon_name === "") {
+                key.innerHTML = "";
+                key.onclick = () =>  {
+                    console.log(" ");
+                    insertCharacter(" ");
+                    pmcp_displayInitialKeys();
+                };
+            } else {
+                key.innerHTML = icon_name !== "" ? `<img width="72" height="72" src="folder_icons/${icon_name}">` : "";
+                key.onclick = () => { alert("Not implemented yet"); };
+            }
+        }
+    }
+}
+
 function linmarn_displayInitialKeys() {
     for (let x = 0; x < 8; x++) {
         for (let y = 0; y < 4; y++) {
@@ -31,8 +75,16 @@ function linmarn_displayInitialKeys() {
                 "3_ノ一.svg", "3_常.svg", "3_ノノ.svg", "3_之.svg", "3_四.svg", "3_ヒクカ丹.svg", "3_天.svg", "3_神十位.svg",
                 "key_xon.png", "4_火心.svg", "4_再.svg", "4_ヽヽ.svg", "5_反.svg", "5_フ.svg", "5_傾.svg", "5_針.svg"
             ][y * 8 + x];
-            key.innerHTML = icon_name !== "" ? `<img width="72" height="72" src="folder_icons/${icon_name}">` : "";
-            key.onclick = () => linmarn_afterFirstStroke({ 'x': x, 'y': y });
+            if (icon_name === "key_pmcp.png") {
+                key.innerHTML = `<img width="72" height="72" src="folder_icons/${icon_name}">`;
+                key.onclick = () => pmcp_displayInitialKeys();
+            } else if (icon_name === "key_xon.png") {
+                key.innerHTML = `<img width="72" height="72" src="folder_icons/${icon_name}">`;
+                key.onclick = () => { alert("Not implemented yet"); };
+            } else {
+                key.innerHTML = icon_name !== "" ? `<img width="72" height="72" src="folder_icons/${icon_name}">` : "";
+                key.onclick = () => linmarn_afterFirstStroke({ 'x': x, 'y': y });
+            }
         }
     }
 }
